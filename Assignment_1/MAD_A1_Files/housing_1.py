6a0494f0e25d62acfb7fce9103f6056a1a2284af
Jack Bjerregaard <jack@Jacks-MBP.local>
2025-11-20 15:27:16 +0100

    inital

---
+++ Assignment_1/MAD_A1_Files/housing_1.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy
+
+# load data
+train_data = numpy.loadtxt("boston_train.csv", delimiter=",")
+test_data = numpy.loadtxt("boston_test.csv", delimiter=",")
+X_train, t_train = train_data[:,:-1], train_data[:,-1]
+X_test, t_test = test_data[:,:-1], test_data[:,-1]
+# make sure that we have N-dimensional Numpy arrays (ndarray)
+t_train = t_train.reshape((len(t_train), 1))
+t_test = t_test.reshape((len(t_test), 1))
+print("Number of training instances: %i" % X_train.shape[0])
+print("Number of test instances: %i" % X_test.shape[0])
+print("Number of features: %i" % X_train.shape[1])
+
+# (a) compute mean of prices on training set
+
+mean = numpy.mean(t_train)
+print(f"a) Mean house price on training set: ${mean:.2f}k")
+
+# (b) RMSE function
+def rmse(t, tp):
+    return numpy.sqrt(numpy.mean((t-tp)**2)) # begin by finding differences, square the errors, and take the mean of the squared errors and finnaly take the squre root
+
+# Create predictions (all equal to mean)
+predictions = numpy.full_like(t_test, mean)
+
+# Compute RMSE
+test_rmse = rmse(t_test, predictions)
+print(f"b) RMSE on test set: ${test_rmse:.2f}k")
+
+# (c) visualization of results
+plt.figure(figsize=(8, 6))
+plt.scatter(t_test, predictions, alpha=0.5)
+plt.xlabel('True House Prices ($1000s)')
+plt.ylabel('Predicted House Prices ($1000s)')
+plt.title('True vs Predicted House Prices (Mean Model)')
+plt.plot([t_test.min(), t_test.max()],
+         [t_test.min(), t_test.max()],
+         'r--', lw=2, label='Perfect prediction')
+plt.legend()
+plt.grid(True, alpha=0.3)
+plt.savefig('housing_1_scatter.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+
+
